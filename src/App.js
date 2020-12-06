5c678e5c9587d6316684b9ebe483ff42c3f2c91c
Ryan Dimitri Ramos <dimitriramos99@gmail.com>
2020-12-05 23:39:25 -0800

    Added about section and made some styling changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
-import AppNavbar from "./components/app-navbar/app-navbar.component"
-import AppCarousel from "./components/app-carousel/app-carousel.component"
-import TitleMessage from "./components/title-message/title-message.component"
+import AppNavbar from "./components/app-navbar/app-navbar.component";
+import AppCarousel from "./components/app-carousel/app-carousel.component";
+import TitleMessage from "./components/title-message/title-message.component";
+import About from './pages/about/about.component';
+import Container from "react-bootstrap/Container";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Fade from "react-reveal/Fade"
+
 const  App = () => {
   return (
-    <div className="App">
+  <div className="App" style={{ position: "relative" }}>
      <AppNavbar/>
      <AppCarousel/>
      <TitleMessage/>
-    </div>
+        <div>
+        <Jumbotron fluid>
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <About />
+            </Fade>
+          </Container>
+          </Jumbotron>
+        </div>
+  </div>
   );
 }
 export default App;
