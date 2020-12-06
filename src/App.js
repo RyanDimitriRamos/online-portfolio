c1e18d443d7a292f8272e47d8176197035fa8092
Ryan Dimitri Ramos <dimitriramos99@gmail.com>
2020-12-05 16:12:18 -0800

    Implemented the beginning of the navbar, uploading images for later

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import MyNavbar from "./components/my-navbar/mynavbar.component"
+
+const  App = () => {
+  return (
+    <div className="App">
+     <MyNavbar/>
+    </div>
+  );
+}
+export default App;
